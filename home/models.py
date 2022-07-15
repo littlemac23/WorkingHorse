33c4fee03951d65ae7a46abc7eaa8530f0d632be
littlemac23 <mcconnellmatt99@gmail.com>
2022-07-14 21:58:15 -0400

    idk

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+class horseData(models.Model):
+    """docstring fo horseData."""
+
+    name = models.TextField(max_length=500, primary_key=True)
+    acquisitionDate = models.DateField()
+    totalAcquisitionAmount = models.IntegerField()
+    type = models.TextField(max_length=500)
+    place = models.TextField(max_length=500)
+    dispersmentClaim_Sale = models.IntegerField()
+    dispersmentDate = models.DateField()
