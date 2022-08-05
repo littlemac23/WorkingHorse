de91fc25f75ae84e18fb0454561f370a3dca2616
Anthony1001 <anthonyjoaquin39@gmail.com>
2022-08-04 22:05:22 -0400

    fixed things

--- horse/settings.py
+++ horse/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-heroku config:set DISABLE_COLLECTSTATIC=1
+
 
 
 # Default primary key field type
