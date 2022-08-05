e83d7d8473c2e5d1c957217ec4846a1e5930cc8f
Anthony1001 <anthonyjoaquin39@gmail.com>
2022-08-04 21:56:41 -0400

    Revert "heroku stuff"
    This reverts commit 3cdc2f3fa0a7c0666cd2430edbf1e8ded28f4e20.

--- horse/urls.py
+++ horse/urls.py
@@ -16,17 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import views
-
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
-
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
